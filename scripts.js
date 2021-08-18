7305bc55323b4907a225c727bade97e047ec97b1
therossington <camsymonds@outlook.com>
2021-08-18 20:20:21 +1000

    Next commit

--- scripts.js
+++ scripts.js
@@ -1,11 +1,29 @@
 const container = document.querySelector('.container');
+const clearButton = document.querySelector('#clear-button');
 
-for (let i = 1; i <= 256; i++) {
-    const gridItem = document.createElement('div'+i);
-    container.appendChild(gridItem);
-    gridItem.addEventListener('mouseenter', () => {
-        gridItem.style.backgroundColor = 'blue';
-    })
+
+const setColor = 'blue';
+let size = 16;
+
+container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+function createGrid () {
+    for (let i = 0; i < size * size; i++) {
+      const gridItem = document.createElement('div'+i);
+      container.appendChild(gridItem);
+      gridItem.addEventListener('mouseenter', () => {
+          gridItem.style.backgroundColor = setColor;
+      })}
+}
+
+clearButton.addEventListener('click', () => {
+    container.innerHTML = '';
+    size = Number(window.prompt('Enter the grid size'));
+    createGrid();
+})
+
+window.onload = () => {
+    createGrid();
+    size;
 };
-
-
